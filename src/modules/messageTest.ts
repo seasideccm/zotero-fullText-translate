910946c365b0228d155e65b1b6240e393898de9d
sscc <2094558239@qq.com>
2023-09-05 13:50:10 +0800

    font a

--- src/modules/messageTest.ts
+++ src/modules/messageTest.ts
@@ -24,11 +24,19 @@ const _messageHandler = async (event: Event) => {
 
     const _reader = Zotero.Reader.getByTabID(Zotero_Tabs.selectedID);
     ztoolkit.log("_reader._isReaderInitialized：", _reader._isReaderInitialized);
-    await _reader._waitForReader();
+    //await _reader._waitForReader();
+    let loop = 0;
     while (!_reader._iframeWindow) {
+
+        loop += 1;
+        ztoolkit.log("loop: ", loop);
         Zotero.Promise.delay(100);
     }
     const wr = (_reader._iframeWindow as any).wrappedJSObject;
+    wr.addEventListener('DOMContentLoaded', (event: Event) => {
+        ztoolkit.log("event.type: ", type);
+    });
+
     /* 
     
     
