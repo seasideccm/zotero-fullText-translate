46b6cd482f5c8c922c4cfb8688ab3b1a813c81d3
sscc <2094558239@qq.com>
2023-09-05 17:07:34 +0800

    if

--- src/modules/messageTest.ts
+++ src/modules/messageTest.ts
@@ -15,7 +15,7 @@ export function findFontOnPdfLoading() {
 
 const _messageHandler = async (event: Event) => {
     const type = event.type;
-    ztoolkit.log("event.type: ", type,"target:",event.target);
+    ztoolkit.log("event.type: ", type, "target:", event.target);
 
     const tab = Zotero_Tabs._getTab(Zotero_Tabs.selectedID);
     while (tab.tab.type != "reader") {
@@ -24,17 +24,27 @@ const _messageHandler = async (event: Event) => {
 
     const _reader = Zotero.Reader.getByTabID(Zotero_Tabs.selectedID);
     ztoolkit.log("_reader._isReaderInitialized：", _reader._isReaderInitialized);
-    //await _reader._waitForReader();
     let loop = 0;
     while (!_reader._iframeWindow) {
 
         loop += 1;
         ztoolkit.log("loop: ", loop);
-        Zotero.Promise.delay(100);
+        //Zotero.Promise.delay(100);
+        await _reader._waitForReader();
     }
     const wr = (_reader._iframeWindow as any).wrappedJSObject;
-    wr.addEventListener('DOMContentLoaded', (event: Event) => {
+    //await wr.PDFViewerApplication.initializedPromise;
+
+    wr[0].addEventListener('DOMContentLoaded', async (event: Event) => {
         ztoolkit.log("event.type: ", type);
+        loop = 0;
+        while (!wr.if) {
+            loop += 1;
+            ztoolkit.log("loop: ", loop);
+            await Zotero.Promise.delay(100);
+            //await _reader._waitForReader();
+        }
+
     });
 
     /* 
