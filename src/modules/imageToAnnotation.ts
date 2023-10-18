4c00f8d9147bf98f49c1d19418d25796c75809d1
sscc <2094558239@qq.com>
2023-10-18 16:53:09 +0800

    params restricted { }

--- src/modules/imageToAnnotation.ts
+++ src/modules/imageToAnnotation.ts
@@ -34,9 +34,52 @@ export async function imageToAnnotation() {
         "sortIndex": "00000|000030|00675"
     };
     //pdfView._onAddAnnotation(anotation);
-    internalReader._annotationManager.addAnnotation(JSON.parse(JSON.stringify(annotation)));
+    //internalReader._annotationManager.addAnnotation(JSON.parse(JSON.stringify(annotation)));
+    const onaddAnnotation = addAnnotation.bind(internalReader._annotationManager, JSON.parse(JSON.stringify(annotation)));
+    onaddAnnotation();
     const test = "test";
 }
+/* eslint-disable*/
 
+function addAnnotation(annotation: any) {
+    // @ts-ignore
+    if (this._readOnly) {
+        return null;
+    }
 
+    let { color, sortIndex } = annotation;
+    if (!color) {
+        throw new Error(`Missing 'color' property`);
+    }
+    if (!sortIndex) {
+        throw new Error(`Missing 'sortIndex' property`);
+    }
 
+    annotation.pageLabel = annotation.pageLabel || '';
+    annotation.text = annotation.text || '';
+    annotation.comment = annotation.comment || '';
+    annotation.tags = annotation.tags || [];
+    // @ts-ignore
+    annotation.id = this._generateObjectKey();
+    annotation.dateCreated = (new Date()).toISOString();
+    annotation.dateModified = annotation.dateCreated;
+    // @ts-ignore
+    annotation.authorName = this._authorName;
+    // @ts-ignore
+    if (this._authorName) {
+        annotation.isAuthorNameAuthoritative = true;
+    }
+    // Ensure numbers have 3 or less decimal places
+    if (annotation.position.rects) {
+        annotation.position.rects = annotation.position.rects.map(
+            // @ts-ignore
+            rect => rect.map(value => parseFloat(value.toFixed(3)))
+        );
+    }
+    // @ts-ignore
+    this._save(JSON.parse(JSON.stringify(annotation)));
+    // @ts-ignore
+    this.render();
+    return annotation;
+}
+/* eslint-enable */
